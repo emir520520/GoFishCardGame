40e44f3d27a6784463305db49aecc54d39f7e4bc
emir520520 <59895713+emir520520@users.noreply.github.com>
2020-10-08 10:56:52 -0400

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,8 +1,3 @@
-/**
- * SYST 17796 Project Winter 2019 Base code.
- * Students can modify and extend to implement their game.
- * Add your name as a modifier and the date!
- */
 package ca.sheridancollege.project;
 
 import java.util.ArrayList;
@@ -10,7 +5,6 @@ import java.util.ArrayList;
 /**
  * The class that models your game. You should create a more specific
  * child of this class and instantiate the methods given.
- * @author dancye, 2018
  */
 public abstract class Game 
 {
