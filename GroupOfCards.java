8836bd039c5121cc14347a9937dd505bfd07cca0
emir520520 <59895713+emir520520@users.noreply.github.com>
2020-10-08 10:59:54 -0400

    Update GroupOfCards.java

--- GroupOfCards.java
+++ GroupOfCards.java
@@ -1,7 +1,5 @@
 /**
  * SYST 17796 Project Winter 2019 Base code.
- * Students can modify and extend to implement their game.
- * Add your name as a modifier and the date!
  */
 package ca.sheridancollege.project;
 
@@ -12,7 +10,6 @@ import java.util.Collections;
  * A concrete class that represents any grouping of cards for a Game.
  * HINT, you might want to subclass this more than once.
  * The group of cards has a maximum size attribute which is flexible for reuse.
- * @author dancye
  */
 public class GroupOfCards 
 {
